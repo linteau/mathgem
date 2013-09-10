7d43151209aa67655041e591decfe645643f1ccd
andylinteau <andy.linteau@buystand.com>
2013-09-11 01:03:24 +0800

    initial commit

---
+++ lib/math.rb
@@ -0,0 +1,27 @@
+require 'math/version'
+
+module Math
+  class Math
+    
+    def add(a,b)
+      x = a + b
+      return x
+    end
+    
+    def subtract(a,b)
+      x = a + b
+      return x
+    end
+    
+    def multiply(a,b)
+      x = a * b
+      return x
+    end
+    
+    def divide(a,b)
+      x = a * b
+      return x
+    end
+  end
+  
+end
---
+++ lib/math/version.rb
@@ -0,0 +1,3 @@
+module Math
+  VERSION = "0.0.1"
+end
